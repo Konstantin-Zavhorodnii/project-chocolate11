b2bce6634b0ce3b8730749cd7e74945bc8ab5696
NEYROSICK <dhatak24@gmail.com>
2023-03-28 04:18:38 +0300

    Global modal update
    1. Зробив модальне вікно для секції reviews
    2. Зробив модальне вікно для секції subscribe
    3. Додав загальний клас для модалок modal.js
    4. Додав загальний клас для кнопок закриття модалок
    5. Додав деякі css-змінні для кольорів
    6. Додав js випадне меню номерів країн для модалки в index.html

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-header - html
+header - html;
 import './js/slider';
 import './js/scrol';
 import './js/mobile-modal';
 import './js/hero-modal';
-import './js/subscribe-modal';
-main
-
+import './js/modal';
+main;
